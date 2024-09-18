6c10e4bc7b3e8f908201145a49eecdd5679afada
René Behrens <rene.behrens@aol.com>
2024-09-18 15:49:13 +0200

    add Booking Controller and Service

---
+++ backend/src/main/java/de/supercode/superbnb/controller/BookingController.java
@@ -0,0 +1,19 @@
+package de.supercode.superbnb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/bookings")
+public class BookingController {
+
+    //GET /api/bookings: Liste aller Buchungen anzeigen (nur für Administratoren)
+
+
+    //POST /api/bookings: Eine Ferienwohnung buchen
+
+
+    //DELETE /api/bookings/{id}: Eine Buchung stornieren
+
+
+}
