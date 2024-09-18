e7b9e024f479c25b50a946e34790b7107e04269b
René Behrens <rene.behrens@aol.com>
2024-09-18 15:53:19 +0200

    Update BookingController.java

--- backend/src/main/java/de/supercode/superbnb/controller/BookingController.java
+++ backend/src/main/java/de/supercode/superbnb/controller/BookingController.java
@@ -1,11 +1,18 @@
 package de.supercode.superbnb.controller;
 
+import de.supercode.superbnb.services.BookingService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/bookings")
 public class BookingController {
+
+    BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
 
     //GET /api/bookings: Liste aller Buchungen anzeigen (nur für Administratoren)
 
