677be5d43bece7c17fcf52f6d0289865c87cdba1
René Behrens <rene.behrens@aol.com>
2024-09-22 18:16:30 +0200

    Update UserResponseDTO.java

--- backend/src/main/java/de/supercode/superbnb/dtos/user/UserResponseDTO.java
+++ backend/src/main/java/de/supercode/superbnb/dtos/user/UserResponseDTO.java
@@ -1,9 +1,13 @@
 package de.supercode.superbnb.dtos.user;
 
+import de.supercode.superbnb.entities.person.Role;
+
 public record UserResponseDTO(
 
         long id,
         String firstName,
-        String lastName
+        String lastName,
+
+        Role role
 ) {
 }
