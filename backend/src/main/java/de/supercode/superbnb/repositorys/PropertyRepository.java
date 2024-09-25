9ab0466c603e5e8758f4aa61a4eb94d528fe22f6
René Behrens <rene.behrens@aol.com>
2024-09-25 14:09:33 +0200

    Update PropertyRepository.java

--- backend/src/main/java/de/supercode/superbnb/repositorys/PropertyRepository.java
+++ backend/src/main/java/de/supercode/superbnb/repositorys/PropertyRepository.java
@@ -15,6 +15,7 @@ public interface PropertyRepository extends JpaRepository<Property, Long> {
             "(p.address.city = :city OR :city IS NULL) AND " +
             "(p.address.country = :country OR :country IS NULL) AND " +
             "p.guestsCapacity >= :guests AND " +
+            "p.available = true AND " +
             "NOT EXISTS (SELECT b FROM Booking b WHERE b.property = p AND " +
             "(b.checkInDate <= :checkOut AND b.checkOutDate >= :checkIn))")
     Page<Property> findAvailableProperties(@Param("city") String city,
